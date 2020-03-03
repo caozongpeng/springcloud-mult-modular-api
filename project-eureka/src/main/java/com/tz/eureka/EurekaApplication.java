16049ab59c30aec4334c8109a4bec3e8db319765
kyrie <864655735@qq.com>
2020-03-03 22:31:54 +0800

    注册中心配置完成

---
+++ project-eureka/src/main/java/com/tz/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.tz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ * @author KyrieCao
+ * @date 2020/3/3 21:58
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+
+}
