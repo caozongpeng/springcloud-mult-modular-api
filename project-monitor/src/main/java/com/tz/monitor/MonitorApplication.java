ec07e34725048081a4fcf6bf1a9501036ca7bc3b
KyrieCao <864655735@qq.com>
2020-03-04 17:20:29 +0800

    监控中心

---
+++ project-monitor/src/main/java/com/tz/monitor/MonitorApplication.java
@@ -0,0 +1,19 @@
+package com.tz.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 监控中心
+ * @author KyrieCao
+ * @version v1.0.0
+ * @date 2020/3/4 16:04 04
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class);
+    }
+}
