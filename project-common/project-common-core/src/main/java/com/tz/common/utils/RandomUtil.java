c1b66dc2a15584597e4fca80339148e0d8e7cf54
kyrie <864655735@qq.com>
2020-03-05 22:23:31 +0800

    随机数生成工具类新增方法

--- project-common/project-common-core/src/main/java/com/tz/common/utils/RandomUtil.java
+++ project-common/project-common-core/src/main/java/com/tz/common/utils/RandomUtil.java
@@ -1,5 +1,7 @@
 package com.tz.common.utils;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.UUID;
 
 /**
@@ -8,6 +10,13 @@ import java.util.UUID;
  * @date 2020/2/5 23:05
  */
 public class RandomUtil {
+
+    private static char[] chars  = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z'};
+
+    private static char[] numbers = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
     /**
      * 生成32位UUID
@@ -18,6 +27,30 @@ public class RandomUtil {
     public static String UUID32() {
         String str = UUID.randomUUID().toString();
         return str.replaceAll("-", "");
+    }
+
+    /**
+     * 生成随机字符串，包含数字和字母
+     * @param length        长度
+     * @return String
+     * @author KyrieCao
+     * @date 2020/3/5 22:22
+     */
+    public static String randomStr(int length)
+    {
+        return RandomStringUtils.random(length, chars);
+    }
+
+    /**
+     * 生成随机字符串，只包含数字
+     * @param length        长度
+     * @return java.lang.String
+     * @author KyrieCao
+     * @date 2020/3/5 22:22
+     */
+    public static String randomInt(int length)
+    {
+        return RandomStringUtils.random(length, numbers);
     }
 
 }
