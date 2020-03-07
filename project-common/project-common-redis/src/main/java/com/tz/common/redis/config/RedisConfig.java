4eeb266a3175fc9720c91b2a12a254c1dd16a92e
kyrie <864655735@qq.com>
2020-03-07 10:50:20 +0800

    新增RedisConfig类

---
+++ project-common/project-common-redis/src/main/java/com/tz/common/redis/config/RedisConfig.java
@@ -0,0 +1,104 @@
+package com.tz.common.redis.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置类
+ * @author KyrieCao
+ * @date 2020/3/7 10:37
+ */
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    /**
+     * 实例化 RedisTemplate 对象
+     * @return RedisTemplate<String,Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:45
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    /**
+     * 实例化 HashOperations 对象,可以使用 Hash 类型操作
+     * @param redisTemplate
+     * @return HashOperations<String, String, Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:46
+     */
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate)
+    {
+        return redisTemplate.opsForHash();
+    }
+
+    /**
+     * 实例化 ValueOperations 对象,可以使用 String 操作
+     * @param redisTemplate
+     * @return ValueOperations<String, Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:47
+     */
+    @Bean
+    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate)
+    {
+        return redisTemplate.opsForValue();
+    }
+
+    /**
+     * 实例化 ListOperations 对象,可以使用 List 操作
+     * @param redisTemplate
+     * @return ListOperations<String, Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:47
+     */
+    @Bean
+    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate)
+    {
+        return redisTemplate.opsForList();
+    }
+
+    /**
+     * 实例化 SetOperations 对象,可以使用 Set 操作
+     * @param redisTemplate
+     * @return SetOperations<String, Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:48
+     */
+    @Bean
+    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate)
+    {
+        return redisTemplate.opsForSet();
+    }
+
+    /**
+     * 实例化 ZSetOperations 对象,可以使用 ZSet 操作
+     * @param redisTemplate
+     * @return ZSetOperations<String,Object>
+     * @author KyrieCao
+     * @date 2020/3/7 10:49
+     */
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate)
+    {
+        return redisTemplate.opsForZSet();
+    }
+}
