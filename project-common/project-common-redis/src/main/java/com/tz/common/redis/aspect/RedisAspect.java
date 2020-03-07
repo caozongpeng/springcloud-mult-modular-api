25010993d587cbd8a05bfa114ae04e5482d0da43
kyrie <864655735@qq.com>
2020-03-07 11:02:01 +0800

    新增Redis切面类

---
+++ project-common/project-common-redis/src/main/java/com/tz/common/redis/aspect/RedisAspect.java
@@ -0,0 +1,17 @@
+package com.tz.common.redis.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Redis切面类
+ * @author KyrieCao
+ * @date 2020/3/7 10:58
+ */
+@Component
+@Aspect
+public class RedisAspect {
+
+
+
+}
