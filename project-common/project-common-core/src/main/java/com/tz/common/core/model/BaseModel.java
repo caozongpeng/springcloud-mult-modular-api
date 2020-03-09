a41919de14f9f794e171c79c7a558e97f0fa650e
kyrie <864655735@qq.com>
2020-03-09 22:13:00 +0800

    新增实体基类

---
+++ project-common/project-common-core/src/main/java/com/tz/common/core/model/BaseModel.java
@@ -0,0 +1,68 @@
+package com.tz.common.core.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 实体基类
+ *
+ * @author KyrieCao
+ * @date 2020/3/9 22:09
+ */
+@Data
+public class BaseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 搜索值
+     */
+    private String searchValue;
+
+    /**
+     * 创建者
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    /**
+     * 更新者
+     */
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    private String beginTime;
+
+    private String endTime;
+
+    /**
+     * 请求参数
+     */
+    private Map<String, Object> params;
+
+    public Map<String, Object> getParams() {
+        if (params == null) {
+            params = new HashMap<>();
+        }
+        return params;
+    }
+}
--- project-service-api/project-system-api/src/main/java/com/tz/system/model/SysUser.java
+++ project-service-api/project-system-api/src/main/java/com/tz/system/model/SysUser.java
@@ -1,9 +1,12 @@
 package com.tz.system.model;
 
+import com.tz.common.core.model.BaseModel;
+
 /**
  * 用户实体类
  * @author KyrieCao
  * @date 2020/3/9 22:07
  */
-public class SysUser {
+public class SysUser extends BaseModel {
+
 }
