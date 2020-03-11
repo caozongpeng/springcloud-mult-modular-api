9d5e5d1fa0ae9d4c076a28f6ca4a38b01ec29116
KyrieCao <864655735@qq.com>
2020-03-11 14:48:18 +0800

    project-auth配置文件编写&加入i18n消息提示

---
+++ project-service/project-auth/src/main/java/com/tz/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.tz.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证启动类
+ * @author KyrieCao
+ * @version v1.0.0
+ * @date 2020/3/11 14:29 29
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
