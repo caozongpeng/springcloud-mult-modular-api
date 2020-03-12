ed190d23eb08d39c11e5a09fca68f212c11abb4f
kyrie <864655735@qq.com>
2020-03-12 21:31:22 +0800

    新增i18n工具类…&spring获取ben工具类

---
+++ project-common/project-common-core/src/main/java/com/tz/common/utils/spring/SpringUtils.java
@@ -0,0 +1,40 @@
+package com.tz.common.utils.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * spring工具类 方便在非spring管理环境中获取bean
+ *
+ * @author KyrieCao
+ * @date 2020/3/12 21:26
+ */
+@Component
+public class SpringUtils implements BeanFactoryPostProcessor {
+
+    /**
+     * Spring应用上下文环境
+     */
+    private static ConfigurableListableBeanFactory beanFactory;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringUtils.beanFactory = beanFactory;
+    }
+
+    /**
+     * 获取类型为requiredType的对象
+     *
+     * @param clz class对象
+     * @return T
+     * @author KyrieCao
+     * @date 2020/3/12 21:28
+     */
+    public static <T> T getBean(Class<T> clz) throws BeansException {
+        T result = (T) beanFactory.getBean(clz);
+        return result;
+    }
+
+}
