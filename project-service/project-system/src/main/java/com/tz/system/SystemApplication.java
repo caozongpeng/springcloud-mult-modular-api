f2c580f8d465b5c3dbd9687843b77084be5935e3
KyrieCao <864655735@qq.com>
2020-03-16 09:40:11 +0800

    新增system服务模块

---
+++ project-service/project-system/src/main/java/com/tz/system/SystemApplication.java
@@ -0,0 +1,18 @@
+package com.tz.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统模块启动类
+ *
+ * @author KyrieCao
+ * @version v1.0.0
+ * @date 2020/3/16 9:35 35
+ */
+@SpringBootApplication
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
